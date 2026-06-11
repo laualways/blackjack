72eb5a8450efac4a0a88a930a15154e008ea0527
laualways <180106141+laualways@users.noreply.github.com>
2026-06-11 12:21:15 +0100

    feat: add startGame and newCard functions with DOM manipulation

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard] //array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el") // another way it'd be with the querySelector 
+                                             // - in this specific case it would be:
+                                            //  document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame(){
+    renderGame();
 }
 
-console.log(message)
+function renderGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] 
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+  let card = 7
+  sum += card
+  cards.push(card) // push the card to the cards array
+  renderGame();
+}
